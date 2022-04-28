e863e15c56075eb2635bad92b8e728862c3a49ec
un-earthly <vijayalamin@gmail.com>
2022-04-28 14:25:34 -0700

    server initialized

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-{\rtf1}
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 80
+// middle Ware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('server is running')
+})
+
+
+app.listen(port)
